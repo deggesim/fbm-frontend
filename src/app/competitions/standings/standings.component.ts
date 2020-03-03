230fb97bdca330205b89dcf73208ad0a28c40a57
deggesim <simone.degennaro@yahoo.it>
2020-03-03 16:58:22 +0100

    WIP classifiche

--- src/app/competitions/standings/standings.component.ts
+++ src/app/competitions/standings/standings.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Round } from 'src/app/models/round';
+import { TableItem } from 'src/app/models/table-item';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-standings',
@@ -7,9 +11,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StandingsComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  rounds: Round[];
+  selectedRound: Round;
+  table: TableItem[];
 
-  ngOnInit() {
+  constructor(
+    private fb: FormBuilder,
+    private route: ActivatedRoute,
+  ) {
+    this.createForm();
   }
 
+  ngOnInit() {
+    console.log('init RoundsComponent');
+    this.route.data.subscribe(
+      (data) => {
+        this.rounds = data.rounds;
+      }
+    );
+  }
+
+  onChange(round: Round) {
+    this.selectedRound = round;
+    this.loadTable();
+  }
+
+  createForm() {
+    this.form = this.fb.group({
+      round: [undefined, Validators.required],
+    });
+  }
+
+  reset() {
+    this.form.reset();
+    this.selectedRound = null;
+  }
+
+  loadTable() {
+    if (this.selectedRound.roundRobin) {
+      // TODO
+    }
+  }
 }
