7f1bc7da0ed2dbea73891c5c26fe37d418a0b61c
Simone De Gennaro <simone.degennaro@yahoo.it>
2020-06-08 17:42:14 +0200

    Always send index.html to handle PathLocationStrategy routing

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const Koa = require('koa');
 const serve = require('koa-static');
+const send = require('koa-send');
 
 const app = new Koa();
 
 app.use(serve('dist'));
+
+// always send index.html to handle PathLocationStrategy routing
+// https://link.medium.com/vHIZGVzA96
+app.use(async (ctx) => {
+  await send(ctx, 'dist/index.html');
+});
+
 app.listen(process.env.PORT || 5000);
