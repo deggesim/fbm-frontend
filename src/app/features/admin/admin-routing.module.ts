095fd7309bf211e5840d8a6db36b97669e99fe8a
Simone De Gennaro <simone.degennaro@yahoo.it>
2021-10-28 16:03:18 +0200

    Fix resolver

--- src/app/features/admin/admin-routing.module.ts
+++ src/app/features/admin/admin-routing.module.ts
@@ -101,6 +101,7 @@ const routes: Routes = [
         path: 'players',
         component: PlayerListComponent,
         resolve: {
+          teams: TeamResolverService,
           rosterList: RosterResolverService,
         },
       },
