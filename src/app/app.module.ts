193d4225c97de2bcb93e9306727627d60a3a54a0
Simone De Gennaro <simone.degennaro@yahoo.it>
2022-01-04 17:53:35 +0100

    Fix config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -61,7 +61,6 @@ import { environment } from '../environments/environment';
     CoreModule,
     SharedModule.forRoot(),
     ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: environment.production,
       // Register the ServiceWorker as soon as the app is stable
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000'
