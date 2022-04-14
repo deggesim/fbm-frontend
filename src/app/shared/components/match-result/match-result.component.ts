bf5e426c3256584b6728a78348f1dd3436e520a8
Simone De Gennaro <simone.degennaro@yahoo.it>
2022-04-14 18:00:31 +0200

    homeFactor as input

--- src/app/shared/components/match-result/match-result.component.ts
+++ src/app/shared/components/match-result/match-result.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, Optional } from '@angular/core';
+import { FormGroup, FormGroupDirective } from '@angular/forms';
 import { Fixture } from '@app/models/fixture';
 import { Lineup } from '@app/models/lineup';
 import { Match } from '@app/models/match';
@@ -6,10 +7,19 @@ import { Match } from '@app/models/match';
 @Component({
   selector: 'fbm-match-result',
   templateUrl: './match-result.component.html',
+  styleUrls: ['./match-result.component.scss'],
 })
-export class MatchResultComponent {
+export class MatchResultComponent implements OnInit {
   @Input() fixture: Fixture;
   @Input() match: Match;
   @Input() homeTeamLineup: Lineup[];
   @Input() awayTeamLineup: Lineup[];
+
+  form: FormGroup;
+
+  constructor(@Optional() private rootFormGroup: FormGroupDirective) {}
+
+  ngOnInit(): void {
+    this.form = this.rootFormGroup?.control;
+  }
 }
