38a74674d29c96c277e3677c5928b27915ff5ea1
Simone De Gennaro <simone.degennaro@yahoo.it>
2022-09-08 10:30:40 +0200

    railway.app

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  endpoint: 'https://fbm-server.herokuapp.com',
+  endpoint: 'https://fbm-server-production.up.railway.app',
   vapidPublikKey: 'BEOkSY7JilXkxmpUjGYCXEn4GBGGxMOstB1sKYE7-EwJgrJ7qnsc8n3m6MCR0C3x95Fsg4IadQxYkIZR6OUdgr0',
 };
